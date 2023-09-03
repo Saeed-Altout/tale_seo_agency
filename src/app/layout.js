0ab874d3d77389da32eddb4028c2b3306ef780d2
Saeed-Altout <saeedaltoutpro@gmail.com>
2023-09-04 00:00:26 +0300

    wws

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { ContextProvider } from "@/contexts/ContextProvider";
 
 export const metadata = {
   title: "tale_seo_agency",
-  description: `The Next Generation Payment Method. Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.`,
+  description: `The tale_seo_agency Generation Payment Method. Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.`,
 };
 
 export default function RootLayout({ children }) {
