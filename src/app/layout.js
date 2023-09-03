edee35a5b83400fc3ede032bc5bf682854df4aa3
Saeed-Altout <saeedaltoutpro@gmail.com>
2023-09-03 23:57:49 +0300

    wip

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,23 @@
-"use client";
 import "./globals.css";
 import { Navbar } from "@/components";
 import { ContextProvider } from "@/contexts/ContextProvider";
+
 export const metadata = {
   title: "tale_seo_agency",
-  description: "tale_seo_agency",
+  description: `The Next Generation Payment Method. Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.`,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="overflow-x-hidden font-poppins">
-        <ContextProvider>
+      <ContextProvider>
+        <body className="overflow-x-hidden font-poppins">
           <header>
             <Navbar />
           </header>
           {children}
-        </ContextProvider>
-      </body>
+        </body>
+      </ContextProvider>
     </html>
   );
 }
